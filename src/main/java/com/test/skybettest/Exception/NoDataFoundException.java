77e6ccd9a44446ce72f782a83eb7d5ddc37f3d68
bmakworo <bmakworo@gmail.com>
2021-08-31 14:42:04 +0100

    Added error control

---
+++ src/main/java/com/test/skybettest/Exception/NoDataFoundException.java
@@ -0,0 +1,8 @@
+package com.test.skybettest.Exception;
+
+public class NoDataFoundException extends RuntimeException {
+
+    public NoDataFoundException() {
+        super("No data found");
+    }
+}
---
+++ src/test/java/com/test/skybettest/service/DefaultUserServiceTest.java
@@ -0,0 +1,30 @@
+package com.test.skybettest.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DefaultUserServiceTest {
+
+
+
+    @Test
+    void findAllUsers() {
+    }
+
+    @Test
+    void findUserById() {
+    }
+
+    @Test
+    void createUser() {
+    }
+
+    @Test
+    void updateUser() {
+    }
+
+    @Test
+    void deleteUserById() {
+    }
+}
